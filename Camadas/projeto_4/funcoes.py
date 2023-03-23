0a798f1bf16dd4346c42c0a87b496abfc6cd42d8
Gustavo Oliveira <gustavo.eso@outlook.com.br>
2023-03-23 14:52:34 -0300

    adicionando crc

--- Camadas/projeto_4/funcoes.py
+++ Camadas/projeto_4/funcoes.py
@@ -2,51 +2,8 @@ import numpy as np
 from math import ceil
 import time
 from datetime import datetime
+from crccheck import Crc16
 
-HEAD_server_handshake = bytes([9,1,0,0,0,0,0,0,0,0,0,0])
-
-EOP = b'\xAA\xBB\xCC\xDD'
-
-def atualiza_tempo(tempo_referencia):
-    tempo_atual = time.time()
-    ref = tempo_atual - tempo_referencia
-    return ref
-
-def verifica_eop(head, pacote): # Função para verificar se o payload está correto
-    tamanho = head[2] # Tamanho = 3º byte do head = 0
-    eop = pacote[12+tamanho:]
-    if eop == b'\x01\x02\x03':
-        print('Payload recebido com sucesso, esperando próximo pacote')
-        return True
-    else:
-        print('Payload não recebido corretamente')
-        return False
-
-def verifica_handshake(head, is_server): # Função para verificar se o handshake está correto
-    handshake = head[:2] #Pega os dois primeiros bytes do head
-    delta_tempo = 0
-
-    combinado = bytes([9, 1])
-    if not is_server:
-        combinado = bytes([8, 0])
-    while delta_tempo <= 5:
-        tempo_atual = time.time()
-        if handshake == combinado: 
-            print('Handshake realizado com sucesso')
-            return True
-        delta_tempo = atualiza_tempo(tempo_atual)
-    return False
-
-
-def verifica_ordem(recebido, numero_pacote_atual): # Função usada pelo server para verificar se o pacote está na ordem correta
-    head = recebido[:12]
-    numero_pacote = head[3] # 4º byte do head = número do pacote
-    if numero_pacote == numero_pacote_atual:
-        print('Pacote recebido na ordem correta')
-        return True
-    else:
-        print('Pacote recebido fora de ordem')
-        return False
 
 def monta_payload(info):
     tamanho = len(info)
@@ -62,33 +19,8 @@ def monta_payload(info):
         payloads.append(payload)
     return payloads
 
-def junta_payloads(lista_payloads, tamanho_info, numero_pacotes): # Função para juntar os payloads em um único array e verificar se o número está correto
-    info_total = b''
-    for payload in lista_payloads:
-        info_total += payload
-    
-    if numero_pacotes == tamanho_info:
-        return True
-    else:
-        return False
 
-def trata_head(head):
-    tamanho_payload = head[2]
-    numero_pacote = head[3]
-    numero_total_pacotes = head[4]
-
-    return tamanho_payload, numero_pacote, numero_total_pacotes
-
-def trata_pacote(pacote):
-    tamanho_pacote = len(pacote)
-    head = pacote[:12]
-    tamanho = head[2]
-    payload = pacote[12:12+tamanho]
-    eop = pacote[12+tamanho:]
-
-    return head, payload, eop
-
-def monta_head(h0, h1, h2, h3, h4, h5, h6, h7):
+def monta_head_handshake(h0, h1, h2, h3, h4, h5, h6, h7):
     '''
         Parametros:
         h0 - Tipo de mensagem.
@@ -103,6 +35,30 @@ def monta_head(h0, h1, h2, h3, h4, h5, h6, h7):
     '''
     head = bytes([h0, h1, h2, h3, h4, h5, h6, h7, 0, 0])
     return head
+
+
+def monta_head(h0, h1, h2, h3, h4, h5, h6, h7, payload):
+    '''
+        Parametros:
+        h0 - Tipo de mensagem.
+        h1 - Se tipo for 1: número do servidor. Qualquer outro tipo: livre
+        h2 - Livre.
+        h3 - Número total de pacotes do arquivo.
+        h4 - Número do pacote sendo enviado.
+        h5 - Se tipo for handshake: id do arquivo (crie um para cada arquivo). Se tipo for dados: tamanho do payload.
+        h6 - Pacote solicitado para recomeço quando a erro no envio.
+        h7 - Ultimo pacote recebido com sucesso. (1 se foi sucesso 0 se nao)
+        h8:h9 - CRC (Por ora deixe em branco. Fará parte do projeto 5).
+    '''
+
+    crc = Crc16.calc(payload)
+    crc = int.to_bytes(crc, 2, byteorder='big')
+    crc1 = crc[0]
+    crc2 = crc[1]
+
+    head = bytes([h0, h1, h2, h3, h4, h5, h6, h7, crc1, crc2])
+    return head
+
 
 def log_write(arquivo:str, operacao:str, tipo:int, tamanho:int, pacote_enviado:int=None, total_pacotes:int=None):
     '''
