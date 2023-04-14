20c2abbc02f1ed6bdfd0c6829a88b9e3bb93b986
gustavoeso <gustavo.eso@outlook.com.br>
2023-04-14 12:54:33 -0300

    funcionando projeto 6

--- Camadas/projeto_6/client/client.ino
+++ Camadas/projeto_6/client/client.ino
@@ -1,53 +1,54 @@
 #include <Arduino.h>
 
-byte mensagem = 0x0A; //Mensagem a ser enviada (A)
+byte mensagem = 0xAA; //Mensagem a ser enviada (A)
 int mensagemBin = int(mensagem); // Transformando mensagem em inteiro
 int clientPin = 8; //Pin a ser utilizado (8)
-float baudrate = 9600; //Boudrate fixo em 9600
+float baudrate = 9600; //Baudrate fixo em 9600
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(clientPin, OUTPUT);
-  Serial.begin(baudrate);
+  Serial.begin(9600);
   digitalWrite(clientPin, HIGH);
-
 }
 
-float timeSkipper(float skipTime = 1, float baudrate = 9600, float t_0 = 0) {
-  double clock = 1 / (21*pow(10,6)); // f = 21MHz, T = 1 / f
-  double T = 1 / baudrate; // tempo entre clocks
-  int n_clocks = floor(T / clock) + 1;
-  for (int i = 0; i < int(n_clocks * skipTime); i++){
-    asm("NOP"); // Espera 
-  }
+void meio_periodo(void)
+{
+for(int i =0; i< 1093;i++) { asm("NOP");}
 }
+
+void periodo(void)
+{
+  meio_periodo();
+  meio_periodo();
+ }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   int ums = 0;
+  Serial.println("inicio loop");
 
   digitalWrite(clientPin, 0); //bit de inicialização
-  timeSkipper();
+  periodo();
 
-
-  for(int i = 0; i <8; i++){
+  for(int i = 0; i < 8; i++){
     int bitAtual = 1 & (mensagemBin >> i);
     digitalWrite(clientPin, bitAtual);
     if (bitAtual == 1){
       ums++;
     }
-    timeSkipper();
+    periodo();
   }
+  
 
   int bitParidade = ums % 2;
   digitalWrite(clientPin, bitParidade);
-  timeSkipper();
+  periodo();
 
   digitalWrite(clientPin, 1); //bit de parada
-  timeSkipper();
+  periodo();
 
-  Serial.print("Dado enviado: ");
   Serial.println(mensagem, HEX);
   delay(2000);
-
-}
+ }
